658e3d4753b473dbac2357ead5094abf0c7b96db
karuppsamy-k <karuppasamy.k.official@gmail.com>
2025-11-21 12:36:25 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.20"
     kotlin("plugin.serialization") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
@@ -27,9 +28,12 @@ application {
     mainClass.set("org.example.ApplicationKt")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
 }
+
 kotlin {
     jvmToolchain(17)
 }
